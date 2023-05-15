8986c05501bc9ff4062701849570612b88e490c1
Balaji Kamalesh <abalajikamalesh@gmail.com>
2023-05-15 23:04:36 +0530

    Add Tweets Page and Profile Page

---
+++ src/server/api/routers/tweet.ts
@@ -0,0 +1,137 @@
+import { z } from "zod";
+
+import { prisma } from "~/server/db";
+import { cursorTo } from "readline";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+  createTRPCContext,
+} from "~/server/api/trpc";
+import { Prisma } from "@prisma/client";
+import { inferAsyncReturnType } from "@trpc/server";
+
+export const tweetRouter = createTRPCRouter({
+  infiniteProfileFeed: protectedProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+        limit: z.number().optional(),
+        cursor: z.object({ id: z.string(), createdAt: z.date() }).optional(),
+      })
+    )
+    .query(async ({ input: { userId, limit = 10, cursor }, ctx }) => {
+      return await getInfiniteTweets({
+        whereClause: { userId },
+        ctx,
+        limit,
+        cursor,
+      });
+    }),
+  infiniteFeed: publicProcedure
+    .input(
+      z.object({
+        onlyFollowing: z.boolean().optional(),
+        limit: z.number().optional(),
+        cursor: z.object({ id: z.string(), createdAt: z.date() }).optional(),
+      })
+    )
+    .query(
+      async ({ input: { onlyFollowing = false, limit = 10, cursor }, ctx }) => {
+        const currentUserId = ctx.session?.user.id;
+        return await getInfiniteTweets({
+          whereClause:
+            currentUserId == null || !onlyFollowing
+              ? undefined
+              : {
+                  user: {
+                    followers: { some: { id: currentUserId } },
+                  },
+                },
+          ctx,
+          limit,
+          cursor,
+        });
+      }
+    ),
+  create: protectedProcedure
+    .input(z.object({ content: z.string() }))
+    .mutation(async ({ input: { content }, ctx }) => {
+      const tweet = await ctx.prisma.tweet.create({
+        data: { content, userId: ctx.session.user.id },
+      });
+
+      void ctx.revalidateSSG(`/profiles/${ctx.session.user.id}`);
+
+      return tweet;
+    }),
+  toggleLike: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ input: { id }, ctx }) => {
+      const data = { tweetId: id, userId: ctx.session.user.id };
+      const existingLike = await ctx.prisma.likes.findUnique({
+        where: { userId_tweetId: data },
+      });
+      if (existingLike) {
+        await ctx.prisma.likes.delete({ where: { userId_tweetId: data } });
+        return { addedLike: false };
+      } else {
+        await ctx.prisma.likes.create({ data });
+        return { addedLike: true };
+      }
+    }),
+});
+
+async function getInfiniteTweets({
+  whereClause,
+  ctx,
+  limit,
+  cursor,
+}: {
+  whereClause?: Prisma.TweetWhereInput;
+  limit: number;
+  cursor: { id: string; createdAt: Date } | undefined;
+  ctx: inferAsyncReturnType<typeof createTRPCContext>;
+}) {
+  const currentUserId = ctx.session?.user.id;
+  const tweets = await ctx.prisma.tweet.findMany({
+    take: limit + 1,
+    cursor: cursor ? { createdAt_id: cursor } : undefined,
+    orderBy: [{ createdAt: "desc" }, { id: "desc" }],
+    where: whereClause,
+    select: {
+      id: true,
+      content: true,
+      createdAt: true,
+      _count: {
+        select: { likes: true },
+      },
+      likes:
+        currentUserId == null ? false : { where: { userId: currentUserId } },
+      user: {
+        select: { name: true, id: true, image: true },
+      },
+    },
+  });
+  let nextCursor: typeof cursor | undefined;
+
+  if (tweets.length > limit) {
+    const nextItem = tweets.pop();
+    if (nextItem != null) {
+      nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt };
+    }
+  }
+  return {
+    tweets: tweets.map((tweet) => {
+      return {
+        id: tweet.id,
+        content: tweet.content,
+        createdAt: tweet.createdAt,
+        likeCount: tweet._count.likes,
+        user: tweet.user,
+        likedByMe: tweet.likes?.length > 0,
+      };
+    }),
+    nextCursor,
+  };
+}
